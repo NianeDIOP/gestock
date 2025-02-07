7421cc8e6247c5769047ac19499f2199b78fa8bf
NianeDIOP <pointcom93@gmail.com>
2025-02-07 21:42:43 +0100

    fix: correction des graphiques et leur actualisation - Ajout de la fonction destroyAllCharts - Correction du scope des variables - Amélioration gestion des erreurs - Optimisation chargerement des données

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AYIB DIOP - @yield('title')</title>
+    <title>GESTOCK - @yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css" rel="stylesheet">
